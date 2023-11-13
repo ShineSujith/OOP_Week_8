c55027dd267d62f1bf2b6128dc309333b5d62247
Shine Sujith <G00414454@atu.ie>
2023-11-13 16:51:41 +0000

    optimised some code

--- src/main/java/ie/atu/UsingTryCatch.java
+++ src/main/java/ie/atu/UsingTryCatch.java
@@ -4,8 +4,7 @@ public class UsingTryCatch {
     public static void main(String[] args){
         Scanner myscan = new Scanner(System.in);
 
-        int check = 0;
-        while(check == 0) { //allows the user to keep entering values
+        while(true) { //allows the user to keep entering values
             System.out.println("Please enter a number between 0 - 9:");
             try {
                 //read user input as a sting
@@ -17,14 +16,14 @@ public class UsingTryCatch {
                 //check if the number is between 0 and 9
                 if (myNum>= 0 && myNum<=9){
                     System.out.println("You entered " + myNum);
-                    check = 1; //ends the loop if a valid number is inputted
+                    break;
                 }
                 else{
                     System.out.println("Not a valid number");
                 }
             }
             catch (NumberFormatException w) {
-                System.out.println("not a valid input");
+                System.out.println("not a valid input, must be an number value");
             }
         }
     }
